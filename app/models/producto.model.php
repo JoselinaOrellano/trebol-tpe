9d8e60f68c51f7ca2c29b5caa34abdd8ccee3d7c
JoselinaOrellano <joselinaorellanog@gmail.com>
2024-09-24 15:56:53 -0300

    creacion del arbol de la pagina y la conexion con la base de datos

---
+++ app/models/producto.model.php
@@ -0,0 +1,23 @@
+<?php
+
+class ProductoModel{
+    //conexion a la db
+
+    private function crearConexion(){
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $database = 'trebol_muebleria';
+
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password);
+        } catch (\Throwable $th) {
+            die($th);
+        }
+        return $pdo;
+
+
+    }
+}
+
+?>
