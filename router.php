eb1ca49deaa70754fd7f1285eb55354a6ba8db73
Juan Pedro Zugbi <jzugbi@gmail.com>
2024-09-26 18:51:57 -0300

    se actualizó la tabla de ruteo

--- router.php
+++ router.php
@@ -4,14 +4,37 @@ require_once 'app/controllers/producto.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'home'; // por defecto home
+$action = 'inicio'; // por defecto inicio
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
 $params = explode('/', $action);
 
-
+switch ($params[0]) {
+    case 'inicio':
+        $controller = new ProductoController();
+        $controller->mostrarProductos();
+        break;
+    case 'nuevoProducto':
+        $controller = new ProductoController();
+        $controller->insertarProducto();
+        break;
+    case 'modificarProducto':
+        $controller = new ProductoController();
+        $controller->modificarProducto();
+        break;
+    case 'eliminarProducto':
+        $controller = new ProductoController();
+        $controller->eliminarProducto();     
+        break;
+    case 'contacto':
+        // PREGUNTAR COMO DIRECCIONAR ???? 
+        break;
+    default:
+        echo "404 not found";
+        break;
+}
 
 
 ?>
