aa0782ee51ea67870771b608d027946aad14d1ce
Juan Pedro Zugbi <jzugbi@gmail.com>
2024-10-04 20:08:07 -0300

    se agrego db con tabla usuarios

--- db/trebol_muebleria.sql
+++ db/trebol_muebleria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-09-2024 a las 00:27:18
+-- Tiempo de generación: 05-10-2024 a las 01:06:07
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,20 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `material`
+-- Estructura de tabla para la tabla `materiales`
 --
 
-CREATE TABLE `material` (
+CREATE TABLE `materiales` (
   `id_material` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `proveedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `material`
+-- Volcado de datos para la tabla `materiales`
 --
 
-INSERT INTO `material` (`id_material`, `nombre`, `proveedor`) VALUES
+INSERT INTO `materiales` (`id_material`, `nombre`, `proveedor`) VALUES
 (1, 'pino', 1),
 (2, 'melamina', 1),
 (5, 'chenille', 2),
@@ -46,10 +46,10 @@ INSERT INTO `material` (`id_material`, `nombre`, `proveedor`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `producto`
+-- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE `producto` (
+CREATE TABLE `productos` (
   `id_producto` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `precio` float NOT NULL,
@@ -59,57 +59,91 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `producto`
+-- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `producto` (`id_producto`, `nombre`, `precio`, `descripcion`, `imagen`, `id_material`) VALUES
-(3, 'mesa de 6 lugares', 200981, 'mesa de algarrobo de 180x160 mts.', 'img/mesa1.jfif', 6),
-(4, 'sillón de 2 cuerpos', 106985, 'sillón de 2 cuerpos forrado con chenille gris, totalmente lavable.', 'img/sillon1.jfif', 5),
-(5, 'silla de pino', 50000, 'silla de pino reforzada por unidad', 'img/silla1.jfif', 1),
-(6, 'mesa de tv', 402970, 'mesa de tv hecha en melamina', 'img/mesatv.jfif', 2);
+INSERT INTO `productos` (`id_producto`, `nombre`, `precio`, `descripcion`, `imagen`, `id_material`) VALUES
+(3, 'mesa de 6 lugares', 200981, 'mesa de algarrobo de 180x160 mts.', 'img/mesa1.jpg', 6),
+(4, 'sillón de 2 cuerpos', 106985, 'sillón de 2 cuerpos forrado con chenille gris, totalmente lavable.', 'img/sillon1.jpg', 5),
+(5, 'silla de pino', 50000, 'silla de pino reforzada por unidad', 'img/silla1.jpg', 1),
+(6, 'mesa de tv', 402970, 'mesa de tv hecha en melamina', 'img/mesatv1.jpg', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuarios` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `password` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuarios`, `nombre`, `apellido`, `email`, `nombre_usuario`, `password`) VALUES
+(2, 'dsf', 'dsf', 'dsf', 'euge', '$2y$10$0iRwjGE5gPGgsJJ82n97R.dAqj6ZlDYnqs/PcAcs6aNpHDz85IOP2');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `material`
+-- Indices de la tabla `materiales`
 --
-ALTER TABLE `material`
+ALTER TABLE `materiales`
   ADD PRIMARY KEY (`id_material`);
 
 --
--- Indices de la tabla `producto`
+-- Indices de la tabla `productos`
 --
-ALTER TABLE `producto`
+ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_producto`),
   ADD KEY `id_material` (`id_material`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuarios`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `material`
+-- AUTO_INCREMENT de la tabla `materiales`
 --
-ALTER TABLE `material`
+ALTER TABLE `materiales`
   MODIFY `id_material` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT de la tabla `producto`
+-- AUTO_INCREMENT de la tabla `productos`
 --
-ALTER TABLE `producto`
+ALTER TABLE `productos`
   MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `producto`
+-- Filtros para la tabla `productos`
 --
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_material`) REFERENCES `material` (`id_material`);
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_material`) REFERENCES `materiales` (`id_material`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
