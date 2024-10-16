102288fcf30a969dad24ee237a007dbf8500a1de
Juan Pedro Zugbi <jzugbi@gmail.com>
2024-10-15 23:02:00 -0300

    se creo categoria.tpl y se modificó funcionalidad mostrar categoria

--- app/views/producto.view.php
+++ app/views/producto.view.php
@@ -26,16 +26,23 @@ class ProductoView {
     }
     
     public function modificarProducto($producto, $materiales, $logueado){
-    $this->smarty->assign('logueado', $logueado);
-    $this->smarty->assign('producto', $producto);
-    $this->smarty->assign('materiales', $materiales);
-    $this->smarty->display('formulario_modificarProducto.tpl');
+        $this->smarty->assign('logueado', $logueado);
+        $this->smarty->assign('producto', $producto);
+        $this->smarty->assign('materiales', $materiales);
+        $this->smarty->display('formulario_modificarProducto.tpl');
     }
 
     public function agregarProducto($materiales, $logueado){
-    $this->smarty->assign('materiales', $materiales);
-    $this->smarty->assign('logueado', $logueado);
-    $this->smarty->display('formulario_agregarProducto.tpl');
+        $this->smarty->assign('materiales', $materiales);
+        $this->smarty->assign('logueado', $logueado);
+        $this->smarty->display('formulario_agregarProducto.tpl');
+    }
+    public function mostrarCategoria($productos, $materiales, $material, $logueado){
+        $this->smarty->assign('logueado', $logueado);
+        $this->smarty->assign('productos', $productos);
+        $this->smarty->assign('materiales', $materiales);
+        $this->smarty->assign('material', $material);
+        $this->smarty->display('categoria.tpl');    
     }
 }
 ?>
